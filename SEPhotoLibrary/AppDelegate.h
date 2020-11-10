30280a8eefebf9d14acb2541d7a20c80888c039a
wc <864009759@qq.com>
2020-11-10 20:13:36 +0800

    Initial Commit

---
+++ SEPhotoLibrary/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SEPhotoLibrary
+//
+//  Created by wenchang on 2020/11/10.
+//  Copyright © 2020 seeEmil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ SEPhotoLibrary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SEPhotoLibrary
+//
+//  Created by wenchang on 2020/11/10.
+//  Copyright © 2020 seeEmil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
